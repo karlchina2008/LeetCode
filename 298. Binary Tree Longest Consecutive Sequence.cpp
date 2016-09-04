55cd2cefa7b0d60a36e2f91a20a914208c77b719
karlchina2008 <hluo41@gatech.edu>
2016-09-04 18:57:29 -0400

    Update 298. Binary Tree Longest Consecutive Sequence.cpp

--- 298. Binary Tree Longest Consecutive Sequence.cpp
+++ 298. Binary Tree Longest Consecutive Sequence.cpp
@@ -1,7 +1,8 @@
 /*
 Given a binary tree, find the length of the longest consecutive sequence path.
 
-The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The longest consecutive path need to be from parent to child (cannot be the reverse).
+The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections.
+The longest consecutive path need to be from parent to child (cannot be the reverse).
 
 For example,
    1
