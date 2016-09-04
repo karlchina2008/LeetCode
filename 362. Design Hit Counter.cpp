ed89baa5668112fd0ac8df701147559674a72901
karlchina2008 <hluo41@gatech.edu>
2016-09-04 16:07:34 -0400

    Update 362. Design Hit Counter.cpp

--- 362. Design Hit Counter.cpp
+++ 362. Design Hit Counter.cpp
@@ -8,34 +8,22 @@ It is possible that several hits arrive roughly at the same time.
 class HitCounter {
 public:
     /** Initialize your data structure here. */
+    multiset<int>S;
     HitCounter() {
-        record.emplace_back(0,0);
+        
     }
     
     /** Record a hit.
         @param timestamp - The current timestamp (in seconds granularity). */
     void hit(int timestamp) {
-        if(record.empty())
-            record.emplace_back(timestamp,1);
-        else{
-            if(timestamp==record.back().first)
-                record[record.size()-1].second++;
-            else
-                record.emplace_back(timestamp,record.back().second+1);
-        }
+        S.insert(timestamp);
     }
     
     /** Return the number of hits in the past 5 minutes.
         @param timestamp - The current timestamp (in seconds granularity). */
     int getHits(int timestamp) {
-        int begin_time=timestamp-300;
-        auto it=lower_bound(record.begin(),record.end(),begin_time,[](pair<int,int> a,int b){return a.first<b;});
-        if((*it).first==begin_time)
-            return record.back().second-(*it).second;
-        else{
-            return record.back().second-(*(--it)).second;
-        }
+        int span = max(0, timestamp - 300);
+        auto it = S.upper_bound(span), it2 = S.upper_bound(timestamp);
+        return distance(it, it2);
     }
-private:
-    vector<pair<int,int>> record;
 };
