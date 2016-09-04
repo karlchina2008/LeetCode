d28c3e74e586d0f81a8abf4d63ccb62b5b7b4afd
karlchina2008 <hluo41@gatech.edu>
2016-09-04 18:34:49 -0400

    map

---
+++ 325. Maximum Size Subarray Sum Equals k.cpp
@@ -0,0 +1,23 @@
+/*
+Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
+*/
+class Solution {
+public:
+    int maxSubArrayLen(vector<int>& nums, int k) {
+        unordered_map<int, int> sums;
+        int cur_sum = 0;
+        int max_len = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            cur_sum += nums[i];
+            if (cur_sum == k) {
+                max_len = i + 1;
+            } else if (sums.find(cur_sum - k) != sums.end()) {
+                max_len = max(max_len, i - sums[cur_sum - k]);
+            }
+            if (sums.find(cur_sum) == sums.end()) {
+                sums[cur_sum] = i;
+            }            
+        }
+        return max_len;
+    }
+};
