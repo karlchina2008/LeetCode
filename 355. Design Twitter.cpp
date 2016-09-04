3a10da85cfe4bb81e263e2ecc03ee6c9321f0e83
karlchina2008 <hluo41@gatech.edu>
2016-09-04 18:12:40 -0400

    Create 355. Design Twitter.cpp

---
+++ 355. Design Twitter.cpp
@@ -0,0 +1,48 @@
+/*
+Design a simplified version of Twitter where users can post tweets, follow/unfollow another user and is able to see the 10 most recent tweets in the user's news feed. Your design should support the following methods:
+
+postTweet(userId, tweetId): Compose a new tweet.
+getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+follow(followerId, followeeId): Follower follows a followee.
+unfollow(followerId, followeeId): Follower unfollows a followee
+*/
+class Twitter {
+private:
+    map<int,set<int>> IDmap;
+    vector<pair<int,int>> Tweetmap;
+public:
+    /** Initialize your data structure here. */
+    Twitter() {
+        
+    }
+    
+    /** Compose a new tweet. */
+    void postTweet(int userId, int tweetId) {
+        Tweetmap.push_back({userId,tweetId});
+    }
+    
+    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+    vector<int> getNewsFeed(int userId) {
+        set<int> temp=IDmap[userId];
+        vector<int> res;
+        temp.insert(userId);
+        int n=Tweetmap.size(),i=n,count=0;
+        while(--i>=0 and count<10){
+            if(temp.find(Tweetmap[i].first)!=temp.end()){
+                res.push_back(Tweetmap[i].second);
+                count++;
+            }
+        }
+        return res;
+    }
+    
+    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+    void follow(int followerId, int followeeId) {
+        IDmap[followerId].insert(followeeId);
+    }
+    
+    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+    void unfollow(int followerId, int followeeId) {
+        IDmap[followerId].erase(followeeId);
+    }
+};
