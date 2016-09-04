83538322171d9f42752d4151019331ec2d2e6802
karlchina2008 <hluo41@gatech.edu>
2016-09-04 15:54:39 -0400

    97%

---
+++ 370. Range Addition.cpp
@@ -0,0 +1,24 @@
+/*
+Assume you have an array of length n initialized with all 0's and are given k update operations.
+
+Each operation is represented as a triplet: [startIndex, endIndex, inc] which increments each element of subarray A[startIndex ... endIndex] (startIndex and endIndex inclusive) with inc.
+
+Return the modified array after all k operations were executed.
+*/
+
+class Solution {
+public:
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
+        vector<int> res, nums(length + 1, 0);
+        for (int i = 0; i < updates.size(); ++i) {
+            nums[updates[i][0]] += updates[i][2];
+            nums[updates[i][1] + 1] -= updates[i][2];
+        }
+        int sum = 0;
+        for (int i = 0; i < length; ++i) {
+            sum += nums[i];
+            res.push_back(sum);
+        }
+        return res;
+    }
+};
