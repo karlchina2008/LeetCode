7095715ee7f947c3ff06e682ef5510e31da7315f
karlchina2008 <hluo41@gatech.edu>
2016-09-04 18:07:26 -0400

    Reference

---
+++ 358. Rearrange String k Distance Apart.cpp
@@ -0,0 +1,36 @@
+/*
+Given a non-empty string str and an integer k, rearrange the string such that the same characters are at least distance k from each other.
+
+All input strings are given in lowercase letters. If it is not possible to rearrange the string, return an empty string ""
+*/
+class Solution {
+public:
+    string rearrangeString(string str, int k) {
+        if(k == 0) return str;
+        int length = (int)str.size(); 
+        
+        string res;
+        unordered_map<char, int> dict;
+        priority_queue<pair<int, char>> pq;
+        
+        for(char ch : str) dict[ch]++;
+        for(auto it = dict.begin(); it != dict.end(); it++){
+            pq.push(make_pair(it->second, it->first));
+        }
+        
+        while(!pq.empty()){
+            vector<pair<int, char>> cache; //store used char during one while loop
+            int count = min(k, length); //count: how many steps in a while loop
+            for(int i = 0; i < count; i++){
+                if(pq.empty()) return "";
+                auto tmp = pq.top();
+                pq.pop();
+                res.push_back(tmp.second);
+                if(--tmp.first > 0) cache.push_back(tmp);
+                length--;
+            }
+            for(auto p : cache) pq.push(p);
+        }
+        return res;
+    }
+};
