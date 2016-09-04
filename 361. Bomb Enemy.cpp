b6e690675dc10e82d0f4ba183aeba138ceee0dda
karlchina2008 <hluo41@gatech.edu>
2016-09-04 16:11:55 -0400

    Reference

--- 361. Bomb Enemy.cpp
+++ 361. Bomb Enemy.cpp
@@ -6,50 +6,24 @@ Note that you can only put the bomb at an empty cell.
 class Solution {
 public:
     int maxKilledEnemies(vector<vector<char>>& grid) {
-        int n=grid.size();
-        if(n==0) return 0;
-        int m=grid[0].size();
-        vector<vector<pair<int,int>>> row(n,vector<pair<int,int>>()),col(m,vector<pair<int,int>>());
-        int sum;
-        for(int i=0;i<n;i++){
-            sum=0;
-            for(int j=0;j<m;j++){
-                if(grid[i][j]!='W'){
-                    if(grid[i][j]=='E')
-                        sum++;
-                }
-                else{
-                    row[i].emplace_back(j,sum);
-                    sum=0;
-                }
+    int m = grid.size(), n = m ? grid[0].size() : 0;
+    int result = 0, rowhits, colhits[n];
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            if (!j || grid[i][j-1] == 'W') {
+                rowhits = 0;
+                for (int k=j; k<n && grid[i][k] != 'W'; k++)
+                    rowhits += grid[i][k] == 'E';
             }
-            row[i].emplace_back(m,sum);
-            //cout<<row[i].back().first<<" "<<row[i].back().second<<endl;
+            if (!i || grid[i-1][j] == 'W') {
+                colhits[j] = 0;
+                for (int k=i; k<m && grid[k][j] != 'W'; k++)
+                    colhits[j] += grid[k][j] == 'E';
+            }
+            if (grid[i][j] == '0')
+                result = max(result, rowhits + colhits[j]);
         }
-        for(int j=0;j<m;j++){
-            sum=0;
-            for(int i=0;i<n;i++){
-                if(grid[i][j]!='W'){
-                    if(grid[i][j]=='E')
-                        sum++;
-                }
-                else{
-                    col[j].emplace_back(i,sum);
-                    sum=0;
-                }
-            }
-            col[j].emplace_back(n,sum);
-            //cout<<col[j].back().first<<" "<<col[j].back().second<<endl;
-        }
-        int res=0;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='0'){
-                    auto it_1=upper_bound(row[i].begin(),row[i].end(),j,[](int val,pair<int,int> x){return val<x.first;});
-                    auto it_2=upper_bound(col[j].begin(),col[j].end(),i,[](int val,pair<int,int> x){return val<x.first;});
-                    res=max(res,it_1->second+it_2->second);
-                }
-            }
-        return res;
     }
+    return result;
+}
 };
