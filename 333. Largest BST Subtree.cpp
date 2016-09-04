d841d3d1a87267a54edcdd70eb424c894cc1112a
karlchina2008 <hluo41@gatech.edu>
2016-09-04 18:32:09 -0400

    Reference

--- 333. Largest BST Subtree.cpp
+++ 333. Largest BST Subtree.cpp
@@ -4,41 +4,40 @@ Given a binary tree, find the largest subtree which is a Binary Search Tree (BST
 Note:
 A subtree must include all of its descendants.
 */
+class Param{
+public:
+    int min, max, n;
+    Param() : min(INT_MAX), max(INT_MIN), n(0) {}
+};
+
 class Solution {
 public:
-int largestBSTSubtree(TreeNode* root) {
-    if(!root) return 0;
-    //unordered_map<TreeNode*,pair<int,bool>> mymap;
-    vector<int> res=help(root);
-    return res.back();
+    int largestBSTSubtree(TreeNode* root) {
+        max_bst = 0;
+        search(root);
+        return max_bst;
     }
-    vector<int> help(TreeNode* root) {
-        //if(!root) return {0,INT_MIN,INT_MAX,0};
-        //cout<<"here"<<endl;
-        if(!root->left and !root->right) return {1,root->val,root->val,1};
-        //int sum=0;
-        vector<int> left(4,0),right(4,0),res(4,0);
-        if(root->left)
-            left=help(root->left);
-        else{
-            left[0]=1;left[1]=left[2]=root->val;left[3]=0;
+private:
+    int max_bst;
+    
+    Param search(TreeNode* root) {
+        Param pm;
+        if (root==NULL) return pm;
+        
+        Param left = search(root->left);
+        Param right = search(root->right);
+        
+        if (left.max<=root->val &&
+            right.min>=root->val) {
+            pm.max = (root->right==NULL) ? root->val : right.max;
+            pm.min = (root->left==NULL) ? root->val : left.min;
+            pm.n = left.n + right.n + 1;
+            if (pm.n>max_bst) max_bst = pm.n;
+        } else {
+            pm.max = INT_MAX;
+            pm.min = INT_MIN;
+            pm.n = 0;
         }
-        if(root->right)
-            right=help(root->right);
-        else{
-            right[0]=1;right[1]=right[2]=root->val;right[3]=0;
-        }
-        if(left[0] and right[0]){
-            if((!root->left or root->val>left[2]) and (!root->right or root->val<right[1])){
-                res[0]=1;res[1]=left[1];res[2]=right[2];res[3]=left[3]+right[3]+1;
-            }
-            else{
-                res[0]=0;res[1]=left[1];res[2]=right[2];res[3]=max(left[3],right[3]);
-            }
-        }
-        else{
-            res[0]=0;res[1]=left[1];res[2]=right[2];res[3]=max(left[3],right[3]);
-        }
-        return res;
+        return pm;
     }
 };
