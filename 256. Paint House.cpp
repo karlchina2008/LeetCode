35a22c38a4ee9c71b1c475993199a9ea0375e359
karlchina2008 <hluo41@gatech.edu>
2016-09-04 19:15:53 -0400

    Create 256. Paint House.cpp

---
+++ 256. Paint House.cpp
@@ -0,0 +1,53 @@
+/*
+There are a row of n houses, each house can be painted with one of the three colors: red, blue or green. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+
+The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, costs[0][0] is the cost of painting house 0 with color red; costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum cost to paint all houses.
+
+Note:
+All costs are positive integers.
+*/
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        int n=costs.size();
+        if(n==0) return 0;
+        int m=costs[0].size();
+        vector<int> d(m,0);
+        int c_1,s_1=INT_MAX,s_2=INT_MAX;
+        for(int i=0;i<m;i++){
+            d[i]=costs[0][i];
+            if(s_1>d[i]){
+                s_1=d[i];c_1=i;
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(i!=c_1 and s_2>d[i]){
+                s_2=d[i];
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(j==c_1)
+                    d[j]=costs[i][j]+s_2;
+                else
+                    d[j]=costs[i][j]+s_1;
+            }
+            s_1=s_2=INT_MAX;
+            for(int j=0;j<m;j++){
+                if(s_1>d[j]){
+                    s_1=d[j];c_1=j;//min value;
+                }
+            }
+            for(int j=0;j<m;j++){
+                if(j!=c_1 and s_2>d[j]){
+                    s_2=d[j];//second min value;
+                }
+            }
+        }
+        int res=INT_MAX;
+        for(int i=0;i<m;i++){
+            res=res>d[i]?d[i]:res;
+        }
+        return res;
+    }
+};
