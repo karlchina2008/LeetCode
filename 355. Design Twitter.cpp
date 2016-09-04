d4de49e29eb3e1d1968fcab59c8bcc3ada54ab94
karlchina2008 <hluo41@gatech.edu>
2016-09-04 18:13:57 -0400

    Reference/heap structure

--- 355. Design Twitter.cpp
+++ 355. Design Twitter.cpp
@@ -6,43 +6,74 @@ getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news fe
 follow(followerId, followeeId): Follower follows a followee.
 unfollow(followerId, followeeId): Follower unfollows a followee
 */
-class Twitter {
-private:
-    map<int,set<int>> IDmap;
-    vector<pair<int,int>> Tweetmap;
+class Twitter
+{
+    struct Tweet
+    {
+        int time;
+        int id;
+        Tweet(int time, int id) : time(time), id(id) {}
+    };
+
+    std::unordered_map<int, std::vector<Tweet>> tweets; // [u] = array of tweets by u
+    std::unordered_map<int, std::unordered_set<int>> following; // [u] = array of users followed by u
+
+    int time;
+
 public:
-    /** Initialize your data structure here. */
-    Twitter() {
-        
+    Twitter() : time(0) {}
+
+    void postTweet(int userId, int tweetId)
+    {
+        tweets[userId].emplace_back(time++, tweetId);
     }
-    
-    /** Compose a new tweet. */
-    void postTweet(int userId, int tweetId) {
-        Tweetmap.push_back({userId,tweetId});
-    }
-    
-    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
-    vector<int> getNewsFeed(int userId) {
-        set<int> temp=IDmap[userId];
-        vector<int> res;
-        temp.insert(userId);
-        int n=Tweetmap.size(),i=n,count=0;
-        while(--i>=0 and count<10){
-            if(temp.find(Tweetmap[i].first)!=temp.end()){
-                res.push_back(Tweetmap[i].second);
-                count++;
-            }
+
+    std::vector<int> getNewsFeed(int userId)
+    {
+        std::vector<std::pair<Tweet*, Tweet*>> h; // pair of pointers (begin, current)
+
+        for (auto& u: following[userId])
+        {
+            auto& t = tweets[u];
+            if (t.size() > 0)
+                h.emplace_back(t.data(), t.data() + t.size() - 1);
         }
-        return res;
+        auto& t = tweets[userId]; // self
+        if (t.size() > 0)
+            h.emplace_back(t.data(), t.data() + t.size() - 1);
+
+        auto f = [](const std::pair<Tweet*, Tweet*>& x, const std::pair<Tweet*, Tweet*>& y) {
+            return x.second->time < y.second->time;
+        };
+        std::make_heap(h.begin(), h.end(), f);
+
+        const int n = 10;
+        std::vector<int> o;
+        o.reserve(n);
+        for (int i = 0; (i < n) && !h.empty(); ++i)
+        {
+            std::pop_heap(h.begin(), h.end(), f);
+
+            auto& hb = h.back();
+            o.push_back(hb.second->id);
+
+            if (hb.first == hb.second--)
+                h.pop_back();
+            else
+                std::push_heap(h.begin(), h.end(), f);
+        }
+        return o;
     }
-    
-    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
-    void follow(int followerId, int followeeId) {
-        IDmap[followerId].insert(followeeId);
+
+    void follow(int followerId, int followeeId)
+    {
+        if (followerId != followeeId)
+            following[followerId].insert(followeeId);
     }
-    
-    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
-    void unfollow(int followerId, int followeeId) {
-        IDmap[followerId].erase(followeeId);
+
+    void unfollow(int followerId, int followeeId)
+    {
+        following[followerId].erase(followeeId);
     }
 };
+
