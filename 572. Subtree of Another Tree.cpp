f393cb78ff9894211e92f98bf9da18d3025a0c85
Hao Luo <hluo41@gatech.edu>
2017-05-06 21:10:22 -0700

    Create 572. Subtree of Another Tree.cpp

---
+++ 572. Subtree of Another Tree.cpp
@@ -0,0 +1,58 @@
+/*
+Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+
+Example 1:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4 
+  / \
+ 1   2
+Return true, because t has the same structure and node values with a subtree of s.
+Example 2:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+Return false.
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        if(s == NULL and t != NULL) return false;
+        if(check(s,t) or isSubtree(s->right,t) or isSubtree(s->left,t)) return true;
+        return false;
+    }
+    
+    bool check(TreeNode* root,TreeNode* cur){
+        if(!root and !cur) return true;
+        if((!root and cur) or (root and !cur)) return false;
+        if(root->val != cur->val) return false;
+        if(!check(root->left,cur->left) or !check(root->right,cur->right)) return false;
+        return true;
+    }
+    
+};
